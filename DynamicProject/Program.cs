7092ffe46582ece6ba90f655d0a4d54d6c9a271d
MattMistele <matthew@mistele.com>
2019-03-10 19:40:09 -0700

    Add input file logic

--- DynamicProject/Program.cs
+++ DynamicProject/Program.cs
@@ -10,11 +10,15 @@ namespace DynamicProject
         static void Main(string[] args)
         {
             Dictionary<string, int> words = DictionaryParser.parseDictionary1("dictionary.txt");
+            string input;
 
+            Console.Write("Enter the name of the input file. Make sure it's in the current directory: ");
+            input = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, Console.ReadLine()));
+
+            Console.WriteLine("Your input is: \n" + input);
 
             // keep the console open at the end so it doesn't immediatly close
             Console.ReadLine();
-            
         }
     }
 }
