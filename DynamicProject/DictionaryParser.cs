6ede9288ade32982530be8678e917844a7d3a1c6
MattMistele <matthew@mistele.com>
2019-03-10 19:39:38 -0700

    Implement 1 character word edge case in DictionaryParser

--- DynamicProject/DictionaryParser.cs
+++ DynamicProject/DictionaryParser.cs
@@ -13,7 +13,7 @@ namespace DynamicProject
         public static Dictionary<string, int> parseDictionary1(string fileName)
         {
             Dictionary<string, int> words = new Dictionary<string, int>();
-            string[] lines = File.ReadAllLines(fileName);
+            string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, fileName));
             int currentScore = 1;
 
             Console.Write("Parsing dictionary.txt into hashmap... ");
@@ -21,17 +21,27 @@ namespace DynamicProject
             // start parsing at the third line, bc the first two lines are just comments
             for(int i = 2; i < lines.Length; i++)
             {
-                if (lines[i].StartsWith("#!comment:"))
+                string currentLine = lines[i];
+
+                if (currentLine.StartsWith("#!comment:"))
                     currentScore += 100;
                 else
                 {
                     // there are repeated words in dictionary.txt (ex "but" on line 23 and line 70)
                     // check this word isn't already in our hashmap
-                    if (!words.ContainsKey(lines[i]))
-                        words.Add(lines[i], currentScore);
+                    // Also, don't add 1 letter words
+                    if (!words.ContainsKey(currentLine) && currentLine.Length > 1)
+                        words.Add(currentLine, currentScore);
                 }
             }
-            Console.Write("Done.");
+
+            // Edge case - 1 letter words
+            words.Add("I", 1);
+            words.Add("A", 1);
+            words.Add("a", 1);
+
+            Console.WriteLine("Done.");
+            Console.WriteLine();
 
             return words;
         }
